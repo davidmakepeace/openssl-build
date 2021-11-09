646809b442e7a012b5d2c4acff2f8c696364c239
David <david.p.makepeace@oracle.com>
2021-11-09 22:27:17 +1000

    Include fips.dll, fipsmodule.cnf in build output.

--- windows/openssl/build_openssl_win64_2019.bat
+++ windows/openssl/build_openssl_win64_2019.bat
@@ -7,7 +7,13 @@ nmake
 if %errorlevel% neq 0 exit /b %errorlevel%
 
 mkdir ..\build
+mkdir ..\build\bin
 mkdir ..\build\lib
+mkdir ..\build\lib\ossl-modules
+mkdir ..\build\ssl
+apps\openssl fipsinstall -out ..\build\ssl\fipsmodule.cnf -module providers\fips.dll
+move apps\openssl.exe ..\build\bin\
 move libcrypto.lib ..\build\lib\
 move libssl.lib ..\build\lib\
 move include ..\build\include
+move providers\fips.dll ..\build\lib\ossl-modules\
