22730b56bd18940d9cc242b24e817fcf171db079
Paul Kehrer <paul.l.kehrer@gmail.com>
2021-11-09 11:30:33 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# OpenSSL Builder
+
+This repository uses GitHub Actions to produce OpenSSL binaries for macOS and windows. It is largely cribbed from the work in pyca/infra.
---
+++ openssl-version.sh
@@ -0,0 +1,6 @@
+export OPENSSL_VERSION="openssl-3.0.0"
+export OPENSSL_SHA256=" 59eedfcb46c25214c9bd37ed6078297b4df01d012267fe9e9eee31f61bc70536"
+# We need a base set of flags because on Windows using MSVC
+# enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
+export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine"
+export OPENSSL_BUILD_FLAGS="${OPENSSL_BUILD_FLAGS_WINDOWS} enable-ec_nistp_64_gcc_128"
