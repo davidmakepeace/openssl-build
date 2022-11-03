263c7409bc82215b1a244fda00529afedc3e08cc
David <david.p.makepeace@oracle.com>
2022-11-03 13:30:30 +1000

    Update to OpenSSL 3.0.7.

--- openssl-version.sh
+++ openssl-version.sh
@@ -1,5 +1,5 @@
-export OPENSSL_VERSION="openssl-3.0.2"
-export OPENSSL_SHA256="98e91ccead4d4756ae3c9cde5e09191a8e586d9f4d50838e7ec09d6411dfdb63"
+export OPENSSL_VERSION="openssl-3.0.7"
+export OPENSSL_SHA256="83049d042a260e696f62406ac5c08bf706fd84383f945cf21bd61e9ed95c396e"
 # We need a base set of flags because on Windows using MSVC
 # enable-ec_nistp_64_gcc_128 doesn't work since there's no 128-bit type
 export OPENSSL_BUILD_FLAGS_WINDOWS="no-ssl3 no-ssl3-method no-zlib no-shared no-comp no-dynamic-engine enable-fips"
